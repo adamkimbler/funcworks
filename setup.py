5e44c065fe6795bd18ea6393b774b22a97a5797a
akimbler <akimb009@fiu.edu>
2020-08-11 13:53:29 -0400

    Enfore `black` style

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 """Setup funcworks installation."""
 from setuptools import setup
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import versioneer
     from funcworks.__about__ import __version__, DOWNLOAD_URL
 
     cmdclass = versioneer.get_cmdclass()
 
     setup(
-        name='funcworks',
+        name="funcworks",
         version=__version__,
         cmdclass=cmdclass,
-        download_url=DOWNLOAD_URL
+        download_url=DOWNLOAD_URL,
     )
